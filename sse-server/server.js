b518f6000cddbed6ce046ecd062e3f3705d7884b
Richard Hanitio <richardhan82@gmail.com>
2025-01-06 15:26:07 +0700

    final commit

--- sse-server/server.js
+++ sse-server/server.js
@@ -8,58 +8,52 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
-app.get("/status", (req, res) => res.json({clients: clients.length}))
 
 const PORT = 3000;
 
 let clients = [];
 let facts = [];
 
-app.listen(PORT, () => {
-  console.log("Facts Events service listening on port", PORT)
-})
+app.get("/status", (req, res) => res.json({clients: clients.length}))
 
-function eventsHandler(req, res, next) {
+app.get("/events", (req, res, next) => {
   const headers = {
     'Content-Type' : 'text/event-stream',
     'Connection' : 'keep-alive',
     'Cache-Control' : 'no-cache'
   };
   res.writeHead(200, headers);
-
   const data = `data: ${JSON.stringify(facts)}\n\n`;
-
   res.write(data);
-
+  
   const clientId = Date.now();
-
   const newClient = {
     id: clientId,
     res
   }
-
   clients.push(newClient);
+  
   res.write(`NEW CLIENT : ${newClient.id}`)
   console.log("LIST CLIENTS: ", clients.map(client => client.id))
-  // console.log("LIST CLIENTS : ", clients)
-
+  
   req.on("close", () => {
     console.log(`${clientId} Connection closed`);
     clients = clients.filter(client => client.id !== clientId);
+    console.log("LIST CLIENTS: ", clients.map(client => client.id))
   })
-}
+})
 
-app.get("/events", eventsHandler)
-
-function sendEventsToAll(newFact) {
-  clients.forEach(client => client.res.write(`data : ${JSON.stringify(newFact)}\n\n`))
-}
-
-async function addFact(req, res, next) {
+app.post("/fact", (req, res, next) => {
   const newFact = req.body;
   facts.push(newFact);
   res.json(newFact)
-  return sendEventsToAll(newFact);
-}
+  return clients.forEach(client => client.res.write(`data : ${JSON.stringify(newFact)}\n\n`))
+});
 
-app.post("/fact", addFact);
+app.listen(PORT, () => {
+  console.log("Facts Events service listening on port", PORT)
+})
+
+
+
+
